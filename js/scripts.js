3726f2e8b7f16149923dd7904f51e65b02876955
Louis-Philip Shahim <louis-philipshahim@LPs-Mac.local>
2023-06-18 20:35:53 +0200

    attempting jquery get age function from chatgpt

--- js/scripts.js
+++ js/scripts.js
@@ -1024,6 +1024,20 @@
     return false;
   });
 
+  /* Get age
+  -------------------------------------------------------*/
+  $(document).ready(function() {
+      var dob = '1993-11-06';
+      var dobDate = new Date(dob);
+      var today = new Date();
+      var age = today.getFullYear() - dobDate.getFullYear();
+      var monthDiff = today.getMonth() - dobDate.getMonth();
+      if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < dobDate.getDate())) {
+        age--;
+      }
+      $('#result').text(age);
+  });
+  
 
 })(jQuery);
 
