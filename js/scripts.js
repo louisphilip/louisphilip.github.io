9e0840614b1d3b50f964f45575f29313491d602a
Louis-Philip Shahim <louis-philipshahim@LPs-Mac.local>
2023-06-18 21:14:36 +0200

    adding manual age

--- js/scripts.js
+++ js/scripts.js
@@ -1024,25 +1024,6 @@
     return false;
   });
 
-  /* Get age
-  -------------------------------------------------------*/
-  $(document).ready(function() {
-    var dobInput = '1993-11-06';
-    var resultDiv = $('#result');
-
-    // Function to calculate age
-    function calculateAge() {
-      var dob = dobInput.val();
-      var dobDate = new Date(dob);
-      var today = new Date();
-      var age = today.getFullYear() - dobDate.getFullYear();
-      var monthDiff = today.getMonth() - dobDate.getMonth();
-      if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < dobDate.getDate())) {
-        age--;
-      }
-      resultDiv.text('Age: ' + age);
-    }  
-
 })(jQuery);
 
 
