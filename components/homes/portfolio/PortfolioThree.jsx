bb829b66c17ad07e045aa66f6df691d2f08c9959
LP <me@louisphilipshahim.com>
2024-05-01 18:32:47 +0200

    updating secrets

--- components/homes/portfolio/PortfolioThree.jsx
+++ components/homes/portfolio/PortfolioThree.jsx
@@ -156,7 +156,7 @@ export async function getStaticProps() {
     return {
       headers: {
         ...headers,
-        authorization: `Bearer ${process.env.GITHUB_ACCESS_TOKEN}`,
+        authorization: `Bearer ${process.env.GH_ACCESS_TOKEN}`,
       }
     }
   });
