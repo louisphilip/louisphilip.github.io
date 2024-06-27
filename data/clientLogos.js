2325cb82db55aef35657c2ce1dc92f2a56a54cd7
LP <me@louisphilipshahim.com>
2024-06-27 22:02:44 +0200

    adding svgs

--- data/clientLogos.js
+++ data/clientLogos.js
@@ -1,7 +1,7 @@
 export const clientLogos = [
   { id: 1, imgSrc: "/assets/img/client/google-cloud.png" },
-  { id: 2, imgSrc: "/assets/img/client/aws.png" },
-  { id: 3, imgSrc: "/assets/img/client/hashicorp.png" },
-  { id: 4, imgSrc: "/assets/img/client/flutter.png" },
-  { id: 5, imgSrc: "/assets/img/client/firebase.png" },
+  { id: 2, imgSrc: "/assets/img/client/aws.svg" },
+  { id: 3, imgSrc: "/assets/img/client/hashicorp.svg" },
+  { id: 4, imgSrc: "/assets/img/client/flutter.svg" },
+  { id: 5, imgSrc: "/assets/img/client/firebase.svg" },
 ];
