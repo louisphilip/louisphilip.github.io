d5eb85033026db63b528034ba3a1f2955689749c
LP <me@louisphilipshahim.com>
2024-06-27 22:08:22 +0200

    typo fixes

--- data/menu.js
+++ data/menu.js
@@ -1,7 +1,7 @@
 export const menuItems = [
   { id: 1, href: "/home-1", icon: "fa-light fa-user", text: "About" },
   { id: 2, href: "/resume", icon: "fa-light fa-file-lines", text: "Resume" },
-  { id: 3, href: "/portfolio", icon: "fa-light fa-briefcase", text: "Works" },
+  { id: 3, href: "/portfolio", icon: "fa-light fa-briefcase", text: "Work" },
   { id: 4, href: "/blog", icon: "fa-brands fa-blogger", text: "Blogs" },
   {
     id: 5,
@@ -14,7 +14,7 @@ export const menuItems = [
 export const menuItemsTwo = [
   { id: 1, href: "/about", icon: "fa-light fa-user", text: "About" },
   { id: 2, href: "/resume-2", icon: "fa-light fa-file-lines", text: "Resume" },
-  { id: 3, href: "/portfolio-2", icon: "fa-light fa-briefcase", text: "Works" },
+  { id: 3, href: "/portfolio-2", icon: "fa-light fa-briefcase", text: "Work" },
   { id: 4, href: "/blog-2", icon: "fa-brands fa-blogger", text: "Blogs" },
   {
     id: 5,
@@ -27,7 +27,7 @@ export const menuItemsTwo = [
 export const menuItemsThree = [
   { id: 1, href: "/", icon: "fa-light fa-user", text: "About" },
   { id: 2, href: "/resume", icon: "fa-light fa-file-lines", text: "Resume" },
-  { id: 3, href: "/portfolio", icon: "fa-light fa-briefcase", text: "Works" },
+  { id: 3, href: "/portfolio", icon: "fa-light fa-briefcase", text: "Work" },
   { id: 4, href: "/blog", icon: "fa-brands fa-blogger", text: "Blogs" },
   {
     id: 5,
