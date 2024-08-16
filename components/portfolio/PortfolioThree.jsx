0af84eee16cfca8613f85d1be1e0dd4cd4acdfca
LP <louisphilipshahim@gmail.com>
2024-08-16 08:56:53 -0300

    feat: updating work naming

--- components/portfolio/PortfolioThree.jsx
+++ components/portfolio/PortfolioThree.jsx
@@ -84,7 +84,7 @@ export default function PortfolioThree() {
       <div className="bostami-page-content-wrap">
         <div className="section-wrapper pl-60 pr-60 pt-60">
           <div className="bostami-page-title-wrap mb-15">
-            <h2 className="page-title">Portfolio</h2>
+            <h2 className="page-title">Github</h2>
           </div>
         </div>
 
