3656107d4d9a83acb9ec18f100b939f69f309c3a
LP <personal_email@example.com>
2024-08-17 11:22:20 -0300

    feat: adding some character to my profile and cleaning up

--- data/jobFeatures.js
+++ data/jobFeatures.js
@@ -2,7 +2,7 @@ export const items = [
   {
     id: 1,
     icon: "/assets/img/design.svg",
-    title: "Cloud Security",
+    title: "Cloud Security 🧑🏻‍💻",
     description:
       "Extensive experience with Cloud Security implementations using native cloud tooling (SCC & GuardDuty), as well as 3rd party integrations with solutions (Cloud Conformity & Palo Alto Prisma) etc.",
     bg: "bg-prink",
@@ -10,7 +10,7 @@ export const items = [
   {
     id: 2,
     icon: "/assets/img/app-development.svg",
-    title: "DevOps Practices",
+    title: "DevOps Practices 🪠",
     description:
       "I am very passionate about workflow automation and effective deployment strategies within public cloud solutions.",
     bg: "bg-catkrill",
@@ -18,7 +18,7 @@ export const items = [
   {
     id: 3,
     icon: "/assets/img/photograph.svg",
-    title: "Certified Cloud Engineer",
+    title: "Certified Cloud Engineer 🥳",
     description:
       "I am well certified within Google Cloud and AWS. I have also successfully achieved Terraform Associate & Terraform CHIP certifications; as well as Vault Associate certification. Looking forward to achieving the Terraform Professional Certification when it is released.",
     bg: "bg-catkrill",
@@ -26,59 +26,9 @@ export const items = [
   {
     id: 4,
     icon: "/assets/img/webdev.svg",
-    title: "Avid Flutter/Firebase Evangelist",
+    title: "Avid Flutter/Firebase Evangelist 😍",
     description:
       "In my spare time I love to experiment and develop mobile applications using Flutter and Firebase.",
     bg: "bg-prink",
-  },
-];
-export const itemsTwo = [
-  {
-    id: 1,
-    icon: "/assets/img/design.svg",
-    title: "Ui/Ux Design",
-    description:
-      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam euismod tincidunt volutpat.",
-    bg: "bg-prink",
-  },
-  {
-    id: 2,
-    icon: "/assets/img/app-development.svg",
-    title: "App Development",
-    description:
-      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam euismod tincidunt volutpat.",
-    bg: "bg-catkrill",
-  },
-  {
-    id: 3,
-    icon: "/assets/img/photograph.svg",
-    title: "Photography",
-    description:
-      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam euismod tincidunt volutpat.",
-    bg: "bg-prink",
-  },
-  {
-    id: 4,
-    icon: "/assets/img/webdev.svg",
-    title: "Web Development",
-    description:
-      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam euismod tincidunt volutpat.",
-    bg: "bg-catkrill",
-  },
-  {
-    id: 5,
-    icon: "/assets/img/management.svg",
-    title: "Management",
-    description:
-      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam euismod tincidunt volutpat.",
-    bg: "bg-prink",
-  },
-  {
-    id: 6,
-    icon: "/assets/img/webdev.svg",
-    title: "Web Development",
-    description:
-      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam euismod tincidunt volutpat.",
-    bg: "bg-catkrill",
   },
 ];
