ab989023e7444915328e3d3e3aed85649477121a
LP <personal_email@example.com>
2024-08-17 12:37:25 -0300

    fix: missing file ref for build

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,4 +1,5 @@
 import About from "@/components/about/About";
+import Header from "@/components/headers/Header"
 import React from "react";
 export const metadata = {
   title: "About || LP Shahim",
@@ -9,7 +10,7 @@ export default function page() {
   return (
     <>
       <div className="page-wrapper page-wrapper-2">
-        <HeaderTwo />
+        <Header />
         <div className="bostami-page-area z-index-3">
           <div className="container">
             <About />
