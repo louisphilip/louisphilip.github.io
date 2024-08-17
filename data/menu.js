9a1a91a957ce4480ac529fa67991691d9c4c5fc2
LP <personal_email@example.com>
2024-08-17 12:14:14 -0300

    feat:updating menu icons

--- data/menu.js
+++ data/menu.js
@@ -2,8 +2,8 @@ export const menuItems = [
   { id: 1, href: "/", icon: "fa-light fa-user", text: "About" },
   { id: 2, href: "/resume", icon: "fa-light fa-file-lines", text: "Resume" },
   // { id: 3, href: "/portfolio", icon: "fa-light fa-briefcase", text: "Work" },
-  { id: 4, href: "/github", icon: "fa-light fa-code", text: "Github" },
-  { id: 5, href: "/blog", icon: "fa-brands fa-blogger", text: "Blogs" },
+  { id: 4, href: "/github", icon: "fa-brands fa-github", text: "Github" },
+  { id: 5, href: "/blog", icon: "fa-brands fa-medium", text: "Medium" },
   {
     id: 6,
     href: "/contact",
