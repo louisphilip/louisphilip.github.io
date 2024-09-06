80695ce2366bba0c825c30cd08b64796b9752c72
louisphilipshahim@gmail.com <personal_email@example.com>
2024-09-06 22:25:03 +0200

    feat: adding GA to site

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,7 +2,7 @@
 
 import { ToastContainer } from 'react-toastify'
 import '../public/main.scss'
-
+import GoogleAnalytics from '@/components/googleAnalytics/GoogleAnalytics'
 
 
 
@@ -10,6 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <GoogleAnalytics measurementId="${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}"/>
       <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto+Slab:wght@300;400;500;600;700;800;900&display=swap"/>
 
